cd4972b0d955a2626ec1cacca58be26c5b7fc78a
natakers <n.kersnovskaya@gmail.com>
2021-11-06 14:28:22 +0300

    fix: add path

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,8 @@
 
 const fs = require('fs');
-const stream = fs.createReadStream('text.txt');
+const path = require('path');
+const pathWay = path.resolve('../HTML-builder/01-read-file', 'text.txt')
+const stream = fs.createReadStream(pathWay);
 let data = '';
 stream.on('data', partData => data += partData);
 stream.on('end', () => console.log(data));
