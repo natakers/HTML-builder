bc3989bad3df53f07697630e2edac15d0e4ca445
natakers <n.kersnovskaya@gmail.com>
2021-11-07 01:06:20 +0300

    fix: delete comments

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,27 +4,15 @@ const process = require('process');
 const pathWay = path.resolve('../HTML-builder/04-copy-directory', 'files-copy')
 const pathCopy = path.resolve('../HTML-builder/04-copy-directory', 'files')
 
-// fs.open(pathWay, 'w', (err) => {
-//     if(err) throw err;
-// });
-// console.log('ffff')
-// fs.mkdir(pathWay+'\\files-copy');
-// let fs = require('fs');
 fs.mkdir(pathWay, {recursive: true}, err => {
-   if(err) /*throw err*/ {
+   if(err)  {
        console.log('Something does wrong')
    }
-//    console.log('Directory init');
-   fs.readdir(pathCopy, {withFileTypes: true}, function(err, items) {   
-    var file
-    // console.log(items)
+   fs.readdir(pathCopy, {withFileTypes: true}, function(err, items) {  
     for (let i=0; i<items.length; i++) {
         if (items[i].isFile()) {
-            // console.log(items)
-            // console.log(file = pathWay + '\\' + items[i].name);
             fs.copyFile(pathCopy + '\\' + items[i].name, pathWay + '\\' + items[i].name, err => {
-                if(err) throw err; // не удалось скопировать файл
-                // console.log('Файл успешно скопирован');
+                if(err) throw err; 
              });
         }
     }
