8cd9dc5095fbbda9a75c11acdc51dd5cf71c46c0
natakers <n.kersnovskaya@gmail.com>
2021-11-06 23:54:22 +0300

    add: add code for 2th and 3th tasks

---
+++ 02-write-file/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+const pathWay = path.resolve('../HTML-builder/02-write-file', 'testFile.txt')
+
+fs.open(pathWay, 'w', (err) => {
+    if(err) throw err;
+});
+
+const readline = require('readline');
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+console.log("Write something ")
+rl.on("line", function (line) {
+  if (line == 'exit') {
+   rl.close()
+  }
+  else {
+  fs.appendFile(pathWay, line, function(error){
+    if(error) throw error;
+  });
+  }
+
+ })
+process.on('exit', function () {
+  console.log("Thanks and bye");
+ });
---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const pathWay = path.resolve('../HTML-builder/03-files-in-folder/secret-folder')
+
+
+fs.readdir(pathWay, {withFileTypes: true}, function(err, items) {   
+    var file
+    for (let i=0; i<items.length; i++) {
+        if (items[i].isFile()) {
+            file = pathWay + '\\' + items[i].name;
+            fs.stat(file, generate_callback(file));
+        }
+    }
+    
+});
+
+function generate_callback(file) {
+    return function(err, stats) {
+            console.log(path.parse(file).name + ' - ' + path.parse(file).ext + " - " + stats.size + 'kb')
+        }
+};
+
