0d812cb82dabdcdda72ad97139bbff7af3bc2ac9
natakers <n.kersnovskaya@gmail.com>
2021-11-07 12:45:03 +0300

    add: add code for 5th task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const pathWay = path.resolve('../HTML-builder/05-merge-styles/project-dist', 'bundle.css')
+const pathCopy = path.resolve('../HTML-builder/05-merge-styles', 'styles')
+fs.open(pathWay, 'w', (err) => {
+    if(err) throw err;
+});
+
+fs.readdir(pathCopy, {withFileTypes: true}, function(err, items) {  
+    const output = fs.createWriteStream(pathWay);
+    let input
+    for (let i=0; i<items.length; i++) {
+        file = pathCopy + '\\' + items[i].name
+        if (items[i].isFile() && path.parse(file).ext == '.css') {
+            input = fs.createReadStream(file);
+            input.pipe(output);
+        }
+    }
+    console.log('Copying is completed');
+});
