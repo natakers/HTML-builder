89608061246320187541ef06f388ffc24458df0d
natakers <n.kersnovskaya@gmail.com>
2021-11-08 17:24:31 +0300

    fix: fix code 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,11 +3,52 @@ const path = require('path');
 const pathWay = path.resolve('../HTML-builder/04-copy-directory', 'files-copy')
 const pathCopy = path.resolve('../HTML-builder/04-copy-directory', 'files')
 
+// if (pathWay) {
+    fs.readdir(pathWay, {withFileTypes: true}, function(err, items1) {   
+        var file1
+        if (items1) {
+            // console.log('not empty')
+            for (let i=0; i<items1.length; i++) {
+                if (items1[i].isFile()) {
+                    file1 = pathWay + '\\' + items1[i].name;
+                    fs.unlink(file1, function(err){
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            // console.log("Файл удалён");
+                        }
+                    });
+                }
+            }
+
+        }
+        
+})
+
+
+
+
 fs.mkdir(pathWay, {recursive: true}, err => {
-   if(err)  {
-       console.log('Something does wrong')
-   }
+    if(err)  {
+     //    console.log('Something does wrong')
+    }
+});
    fs.readdir(pathCopy, {withFileTypes: true}, function(err, items) {  
+    // for (let i=0; i<items.length; i++) {
+    //     if (items[i].isFile()) {
+    //         file = pathWay + '\\' + items[i].name;
+    //         fs.unlink(file, function(err){
+    //             if (err) {
+    //                 console.log(err);
+    //             } else {
+    //                 // console.log("Файл удалён");
+    //             }
+    //         });
+    //     }
+    // }
+    
+    
+    
     for (let i=0; i<items.length; i++) {
         if (items[i].isFile()) {
             fs.copyFile(pathCopy + '\\' + items[i].name, pathWay + '\\' + items[i].name, err => {
@@ -17,5 +58,5 @@ fs.mkdir(pathWay, {recursive: true}, err => {
     }
     console.log('Copying is completed');
 });
-});
+// });
 
